d78094f9b6a68a768a4a39d35f28a3cf6eed233f
Dmytro <dmytropolizhai@gmail.com>
2026-05-06 23:38:52 +0300

    fix(routes): Fix RootLayout import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute } from '@tanstack/react-router'
-import { RootLayout } from '@/layouts/RootLayout'
+import { RootLayout } from '@/layouts/root-layout'
 
 export const Route = createRootRoute({
   component: RootLayout,

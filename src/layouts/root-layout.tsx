e29a473a0f62d8222ee133a85ba114dfe5ad765c
Dmytro Polizhai <dmytropolizhai@gmail.com>
2026-05-07 13:10:02 +0300

    feat(cursor): Hide cursor on mobile devices

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from '@tanstack/react-router'
 import { NavigationBar } from '@/components/navigation-bar'
 import { Footer } from '@/components/footer'
 import { Cursor } from '@/components/cursor'
+import { useIsMobile } from '@/hooks/use-is-mobile';
 
 export function RootLayout() {
+  const isMobile = useIsMobile();
+
   return (
     <>
-      <Cursor />
+      {!isMobile && <Cursor />}
       <NavigationBar />
       <main id="main-content">
         <Outlet />

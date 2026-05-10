e5a6b366bf8e31a13c90ec2926e6ebda64f34db1
dmytropolizhai <dmytropolizhai@users.noreply.github.com>
2026-05-10 19:30:26 +0000

    deploy: 8bb8610513187d4f769b19aa1b69313735296d28

--- assets/_locale-BhIEQJBa.js
+++
@@ -1,1 +0,0 @@
-import{u as s,r as t,i as e,j as o,O as n}from"./index-pd10K9TP.js";function c(){const{locale:a}=s({from:"/$locale"});return t.useEffect(()=>{e.language!==a&&e.changeLanguage(a)},[a]),o.jsx(n,{})}export{c as component};
---
+++ assets/_locale.projects-CW03llFb.js
@@ -0,0 +1,1 @@
+import{j as t,O as o}from"./index-IO56H5b1.js";const n=()=>t.jsx(o,{});export{n as component};
